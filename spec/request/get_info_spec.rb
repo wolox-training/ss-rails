9cc602c30c46e8b9e271220fdb490086caaa0bc6
Sebastian Spadea <sebastian.spadea@wolox.com.ar>
2021-06-11 10:53:33 -0300

    Testing

--- spec/request/get_info_spec.rb
+++ spec/request/get_info_spec.rb
@@ -4,21 +4,36 @@ describe '.info', type: :request do
   context 'when is valid' do
     before do
       stubbed_service = instance_double(OpenLibrary)
-      allow(OpenLibrary).to receive(:new).with(params[:isbn]).and_return(stubbed_service)
+      allow(OpenLibrary).to receive(:new).with('0385472579').and_return(stubbed_service)
       allow(stubbed_service).to receive(:fetch_data).and_return({ 'title' => 'Originals', 'authors' => ['Adam Grant'] })
     end
 
-    expect(response).to respond_with_content_type(:json)
-    it { is_expected.to respond_with 200 }
+    it 'Found book' do
+      get '/api/v1/books/0385472579'
+      expect(response.status).to be(200)
+    end
+
+    it 'Show the book title' do
+      get '/api/v1/books/0385472579'
+      expect(JSON.parse(response.body)['data']['title']).to eql('Originals')
+    end
   end
 
   context 'when is not valid' do
     before do
       stubbed_service = instance_double(OpenLibrary)
-      allow(OpenLibrary).to receive(:new).with(params[:isbn]).and_return(stubbed_service)
+      allow(OpenLibrary).to receive(:new).with('0385472578').and_return(stubbed_service)
       allow(stubbed_service).to receive(:fetch_data).and_return('not found')
     end
 
-    it { is_expected.to respond_with 422 }
+    it 'Book not found' do
+      get '/api/v1/books/0385472578'
+      expect(response.status).to be(422)
+    end
+    
+    it 'Status: unprocessable entity' do
+      get '/api/v1/books/0385472579'
+      expect(JSON.parse(response.body)['status']).to eql('Unprocessable entity')
+    end
   end
 end
