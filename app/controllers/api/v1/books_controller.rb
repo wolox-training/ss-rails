fa82a9070cd8b400d2c9d8073c60024b758cc3c8
Sebastian Spadea <sebastian.spadea@wolox.com.ar>
2021-06-23 17:20:56 -0300

    changed controller

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,15 +1,10 @@
 module Api
   module V1
     class BooksController < ApplicationController
-      before_action :authenticate_user!
       include Wor::Paginate
 
       def index
-        books = Rack::Reducer.call(params, dataset: Book.all, filters: [
-          ->(author:) { where('lower(author) like ?', "%#{author.downcase}%") },
-          ->(genre:) { where('lower(book_genre) like ?', "%#{genre.downcase}%") },
-          ->(title:) { where('lower(title) like ?', "%#{title.downcase}%") }
-        ])
+        books = Book::Reducer.apply(params)
         render_paginated books, each_serializer: BookSerializer
       end
 
