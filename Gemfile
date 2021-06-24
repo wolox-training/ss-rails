03aaed3e1f92e9428dea21dfed1415495f228076
Sebastian Spadea <sebastian.spadea@wolox.com.ar>
2021-06-24 10:06:06 -0300

    testing model

--- Gemfile
+++ Gemfile
@@ -58,6 +58,7 @@ end
 
 group :test do
   gem 'database_cleaner-active_record'
+  gem 'shoulda-matchers', '~> 4.0'
   gem 'webmock'
 end
 
--- spec/models/book_suggestion_spec.rb
+++ spec/models/book_suggestion_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe BookSuggestion, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe BookSuggestion do
+  subject(:book_suggestion) { build(:book_suggestion) }
+
+  describe '#create new book suggestion' do
+    context 'when the suggestion is created' do
+      it 'be valid' do
+        book_suggestion.save!
+        expect(book_suggestion).to be_valid
+      end
+
+      it 'have title, author and editor' do
+        book_suggestion.save!
+        expect(book_suggestion.title).to be_present
+        expect(book_suggestion.author).to be_present
+        expect(book_suggestion.editor).to be_present
+      end
+
+      it 'have year, link and synopsis' do
+        book_suggestion.save!
+        expect(book_suggestion.link).to be_present
+        expect(book_suggestion.year).to be_present
+        expect(book_suggestion.synopsis).to be_present
+      end
+
+      it 'is not valid without a title' do
+        book_suggestion.title = nil
+        book_suggestion.save
+        expect(book_suggestion).not_to be_valid
+      end
+    end
+  end
 end
