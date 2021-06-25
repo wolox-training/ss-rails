a5534633fdca0327cd0f59b7ddd1abcf1794d849
Sebastian Spadea <sebastian.spadea@wolox.com.ar>
2021-06-25 11:24:31 -0300

    create endpoint

--- app/controllers/api/v1/rents_controller.rb
+++ app/controllers/api/v1/rents_controller.rb
@@ -7,12 +7,18 @@ module Api
       end
 
       def create
+        rent = RentService.new(rent_params, current_user).new_rent
+        if rent.save
+          render json: rent, status: :created
+        else
+          render json: { errors: [{ code: '400', message: rent.errors }] }, status: :bad_request
+        end
       end
 
       private
 
       def rent_params
-        params.require(:rent).permit(:user_id, :book_id, :start_of_rent, :end_of_rent)
+        params.require(:rent).permit(:book_id, :start_of_rent, :end_of_rent)
       end
     end
   end
---
+++ app/services/rent_service.rb
@@ -0,0 +1,12 @@
+class RentService
+  def initialize(rent_params, user)
+    @rent_params = rent_params
+    @user = user
+  end
+
+  def new_rent
+    rent = Rent.new(@rent_params)
+    rent.user = @user
+    rent
+  end
+end
