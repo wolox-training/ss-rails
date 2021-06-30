aa2e04e9c1cf5dbf2106af313cb92e201b606474
Sebastian Spadea <sebastian.spadea@wolox.com.ar>
2021-06-30 11:24:13 -0300

    Pundit working

--- app/policies/rent_policy.rb
+++ app/policies/rent_policy.rb
@@ -1,6 +1,7 @@
 class RentPolicy < ApplicationPolicy
   def index?
-    record
+    byebug
+    user[:current_user] == user[:user_params]
   end
 
   def create?
@@ -9,8 +10,7 @@ class RentPolicy < ApplicationPolicy
 
   class Scope < Scope
     def resolve
-      scope.where(user: user)
+      scope.all
     end
   end
 end
-  
