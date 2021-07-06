a556140d6a922a8afb4ae6323be1c568d82cbcc3
Sebastian Spadea <sebastian.spadea@wolox.com.ar>
2021-07-06 17:52:36 -0300

    I18n working

--- app/mailers/rent_mailer.rb
+++ app/mailers/rent_mailer.rb
@@ -1,11 +1,13 @@
 class RentMailer < ApplicationMailer
   def new_rent(rent)
+    I18n.locale = rent.user.locale
     @rent = rent
-    mail(to: @rent.user.email, subject: 'New rent created')
+    mail(to: @rent.user.email, subject: "#{I18n.t 'books.new_rent_created' }!")
   end
 
   def end_of_rent(rent)
+    I18n.locale = rent.user.locale
     @rent = rent
-    mail(to: @rent.user.email, subject: 'Book rental expired!')
+    mail(to: @rent.user.email, subject: "#{I18n.t 'books.book_rental_expired' }!")
   end
 end
