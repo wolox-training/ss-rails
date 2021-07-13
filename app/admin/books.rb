ae0783163aec4ec63001c0ebf12d5f4c95239214
Sebastian Spadea <sebastian.spadea@wolox.com.ar>
2021-07-13 16:32:52 -0300

    active admin

---
+++ app/admin/books.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Book do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :author, :image, :title, :year, :editor, :book_genre, :slug
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:author, :image, :title, :year, :editor, :book_genre, :slug]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ require 'sidekiq/web'
 require 'sidekiq-scheduler/web'
 
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   mount_devise_token_auth_for 'User', at: 'auth'
   mount Sidekiq::Web => '/sidekiq'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
